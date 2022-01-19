2983bbc2dff910004302d755498a0220cfefec4b
Dilyen <davidandohacquah@gmail.com>
2022-01-19 14:28:05 +0000

    added swagger Docs

--- src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
+++ src/main/java/io/turntabl/leaderboardservice/LeaderboardServiceApplication.java
@@ -1,11 +1,13 @@
 package io.turntabl.leaderboardservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition
 public class LeaderboardServiceApplication {
 
 	public static void main(String[] args) {
